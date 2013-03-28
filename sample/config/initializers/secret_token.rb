46b7d20d6951dc9b729b45f77126d3122888a3ea
junkmail22222 <junkmail22222@yahoo.com>
2013-03-28 17:05:01 +1100

    Initial commit

---
+++ sample/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = 'e4c688fe6d4d2ef9f2fe2aa6c0e047c90089e7ffcfb4190e4c4b322566afbe1a75dd7d5837322fd852f207c2248cbc609c6ca1765609d4ca961c4e8916da2c98'
---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '4a4461d0ec175c59e607922a8f8a25399c9f8fb34167284862b4d6ea2e9af0fa9cb37ab4f088b5624c8d5ead92d764d1b8ba05dbd97432f298757b65f175518e'
