6b2e971e4367b416d31f4af101655d3e7724e55c
junkmail22222 <junkmail22222@yahoo.com>
2013-03-28 16:50:45 +1100

    first commit

---
+++ demo_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cf2911feb766b856e94e7fbe240cdd9b1c947e093386ff0dc793fd6991c08e53050bdfdf128a9b44081f56dda9bba904c6afc42fdf209082f74fe5e8f251eb8b'
